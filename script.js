05a1607c62cea2deaacf9e08b1f47d4c72411546
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2023-10-04 00:17:27 +0300

    Render library after adding a book

--- script.js
+++ script.js
@@ -6,22 +6,67 @@ const booksLibrary = [];
 
 // DOM ELEMENTS //
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const buttonOpenModal = document.querySelector(".button--open-modal");
-const buttonCloseModal = document.querySelector(".button--close-modal");
+const modalElement = document.querySelector(".modal");
+const overlayElement = document.querySelector(".overlay");
+const buttonOpenModalElement = document.querySelector(".button--open-modal");
+const buttonCloseModalElement = document.querySelector(".button--close-modal");
+
+const booksElement = document.querySelector(".books");
+const bookFormElement = document.querySelector(".modal__form");
+const bookTitleElement = document.querySelector("#title");
+const bookAuthorElement = document.querySelector("#author");
+const bookPagesElement = document.querySelector("#pages");
+const bookStatusElement = document.querySelector("#status");
+
+// HELPER FUNCTIONS //
+
+const renderLibrary = function () {
+    booksElement.innerHTML = "";
+
+    for (const book of booksLibrary) {
+        const html = `
+        <div class="book ${book.status === "read" ? "book--read" : ""}">
+            <button class="button button--remove">
+                <img src="./images/remove.svg" alt="" class="remove-icon" />
+            </button>
+            <h3 class="book__title">${book.title}</h3>
+            <p class="book__author">${book.author}</p>
+            <span class="book__pages">${book.pages}</span>
+        </div>
+        `;
+
+        booksElement.insertAdjacentHTML("beforeend", html);
+    }
+};
 
 // CALLBACK FUNCTIONS //
 
 const openModal = function (event) {
     event.preventDefault();
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+    modalElement.classList.remove("hidden");
+    overlayElement.classList.remove("hidden");
+    bookTitleElement.focus();
 };
 
 const closeModal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
+    modalElement.classList.add("hidden");
+    overlayElement.classList.add("hidden");
+};
+
+const addBook = function (event) {
+    event.preventDefault();
+
+    const bookTitle = bookTitleElement.value;
+    const bookAuthor = bookAuthorElement.value;
+    const bookPages = bookPagesElement.value;
+    const bookStatus = bookStatusElement.value;
+
+    const book = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+    book.addBookToLibrary();
+    closeModal();
+    bookTitleElement.value = bookAuthorElement.value = bookPagesElement.value = "";
+    bookStatusElement.value = "want";
+    renderLibrary();
 };
 
 // CONSTRUCTION FUNCTIONS //
@@ -39,11 +84,13 @@ Book.prototype.addBookToLibrary = function () {
 
 // EVENT LISTENERS //
 
-buttonOpenModal.addEventListener("click", openModal);
-buttonCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+buttonOpenModalElement.addEventListener("click", openModal);
+buttonCloseModalElement.addEventListener("click", closeModal);
+overlayElement.addEventListener("click", closeModal);
 document.addEventListener("keydown", function (event) {
-    if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    if (event.key === "Escape" && !modalElement.classList.contains("hidden")) {
         closeModal();
     }
 });
+
+bookFormElement.addEventListener("submit", addBook);
