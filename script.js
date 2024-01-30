0b0f6f7dd2d85b1e08d08980edb7dfbc4e4bdced
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-01-30 19:07:41 +0200

    Update repository's README.md

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ const bookStatusElement = document.querySelector("#status");
 // HELPER FUNCTIONS //
 
 const renderLibrary = function () {
-    booksElement.innerHTML = "";
+  booksElement.innerHTML = "";
 
-    for (const book of booksLibrary) {
-        const html = `
+  for (const book of booksLibrary) {
+    const html = `
         <div data-id="${book.id}" class="book ${book.status === "read" ? "book--read" : ""}">
             <div class="buttons">    
                 <button class="button button--edit">
@@ -40,108 +40,108 @@ const renderLibrary = function () {
         </div>
         `;
 
-        booksElement.insertAdjacentHTML("beforeend", html);
-    }
+    booksElement.insertAdjacentHTML("beforeend", html);
+  }
 };
 
 const setLocalStorage = function () {
-    localStorage.setItem("library", JSON.stringify(booksLibrary));
+  localStorage.setItem("library", JSON.stringify(booksLibrary));
 };
 
 const getLocalStorage = function () {
-    const data = JSON.parse(localStorage.getItem("library"));
-    if (!data) return;
+  const data = JSON.parse(localStorage.getItem("library"));
+  if (!data) return;
 
-    booksLibrary = data;
-    renderLibrary();
+  booksLibrary = data;
+  renderLibrary();
 };
 getLocalStorage();
 
 const resetLocalStorage = function () {
-    localStorage.removeItem("library");
-    location.reload();
+  localStorage.removeItem("library");
+  location.reload();
 };
 
 // CALLBACK FUNCTIONS //
 
 const openModal = function (event) {
-    event.preventDefault();
-    modalElement.classList.remove("hidden");
-    overlayElement.classList.remove("hidden");
-    bookTitleElement.focus();
+  event.preventDefault();
+  modalElement.classList.remove("hidden");
+  overlayElement.classList.remove("hidden");
+  bookTitleElement.focus();
 };
 
 const closeModal = function () {
-    modalElement.classList.add("hidden");
-    overlayElement.classList.add("hidden");
+  modalElement.classList.add("hidden");
+  overlayElement.classList.add("hidden");
 };
 
 const addBook = function (event) {
-    event.preventDefault();
-    const bookTitle = bookTitleElement.value;
-    const bookAuthor = bookAuthorElement.value;
-    const bookPages = bookPagesElement.value;
-    const bookStatus = bookStatusElement.value;
+  event.preventDefault();
+  const bookTitle = bookTitleElement.value;
+  const bookAuthor = bookAuthorElement.value;
+  const bookPages = bookPagesElement.value;
+  const bookStatus = bookStatusElement.value;
 
-    const editingBookId = bookFormElement.dataset.editingBookId;
+  const editingBookId = bookFormElement.dataset.editingBookId;
 
-    if (editingBookId) {
-        const book = booksLibrary.find((b) => b.id === editingBookId);
-        book.title = bookTitle;
-        book.author = bookAuthor;
-        book.pages = bookPages;
-        book.status = bookStatus;
-        bookFormElement.removeAttribute("data-editing-book-id");
-    } else {
-        const bookId = String(Date.now()).slice(-10);
-        const book = new Book(bookId, bookTitle, bookAuthor, bookPages, bookStatus);
-        book.addBookToLibrary();
-    }
+  if (editingBookId) {
+    const book = booksLibrary.find((b) => b.id === editingBookId);
+    book.title = bookTitle;
+    book.author = bookAuthor;
+    book.pages = bookPages;
+    book.status = bookStatus;
+    bookFormElement.removeAttribute("data-editing-book-id");
+  } else {
+    const bookId = String(Date.now()).slice(-10);
+    const book = new Book(bookId, bookTitle, bookAuthor, bookPages, bookStatus);
+    book.addBookToLibrary();
+  }
 
-    closeModal();
-    bookTitleElement.value = bookAuthorElement.value = bookPagesElement.value = "";
-    bookStatusElement.value = "want";
-    setLocalStorage();
-    renderLibrary();
+  closeModal();
+  bookTitleElement.value = bookAuthorElement.value = bookPagesElement.value = "";
+  bookStatusElement.value = "want";
+  setLocalStorage();
+  renderLibrary();
 };
 
 const editBook = function (event) {
-    if (event.target.closest(".button--edit")) {
-        const bookElement = event.target.closest(".book");
-        const book = booksLibrary.find((b) => b.id === bookElement.dataset.id);
+  if (event.target.closest(".button--edit")) {
+    const bookElement = event.target.closest(".book");
+    const book = booksLibrary.find((b) => b.id === bookElement.dataset.id);
 
-        bookTitleElement.value = book.title;
-        bookAuthorElement.value = book.author;
-        bookPagesElement.value = book.pages;
-        bookStatusElement.value = book.status;
+    bookTitleElement.value = book.title;
+    bookAuthorElement.value = book.author;
+    bookPagesElement.value = book.pages;
+    bookStatusElement.value = book.status;
 
-        bookFormElement.dataset.editingBookId = book.id;
+    bookFormElement.dataset.editingBookId = book.id;
 
-        openModal(event);
-    }
+    openModal(event);
+  }
 };
 
 const removeBook = function (event) {
-    if (!event.target.closest(".button--remove")) return;
-    const bookElement = event.target.closest(".book");
-    const book = booksLibrary.find((book) => book.id === bookElement.dataset.id);
-    booksLibrary.splice(booksLibrary.indexOf(book), 1);
-    setLocalStorage();
-    renderLibrary();
+  if (!event.target.closest(".button--remove")) return;
+  const bookElement = event.target.closest(".book");
+  const book = booksLibrary.find((book) => book.id === bookElement.dataset.id);
+  booksLibrary.splice(booksLibrary.indexOf(book), 1);
+  setLocalStorage();
+  renderLibrary();
 };
 
 // CONSTRUCTION FUNCTIONS //
 
 const Book = function (id, title, author, pages, status) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 };
 
 Book.prototype.addBookToLibrary = function () {
-    booksLibrary.push(this);
+  booksLibrary.push(this);
 };
 
 // EVENT LISTENERS //
@@ -150,9 +150,9 @@ buttonOpenModalElement.addEventListener("click", openModal);
 buttonCloseModalElement.addEventListener("click", closeModal);
 overlayElement.addEventListener("click", closeModal);
 document.addEventListener("keydown", function (event) {
-    if (event.key === "Escape" && !modalElement.classList.contains("hidden")) {
-        closeModal();
-    }
+  if (event.key === "Escape" && !modalElement.classList.contains("hidden")) {
+    closeModal();
+  }
 });
 
 bookFormElement.addEventListener("submit", addBook);
